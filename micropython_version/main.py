6b26588e3e23e2f3102008994e5fdd26d9e6bb61
charlymolero <perez.moleroc@gmail.com>
2022-07-08 00:10:43 -0300

    experimenting with sockets

--- micropython_version/main.py
+++ micropython_version/main.py
@@ -1,8 +1,10 @@
-from machine import Pin
-from time    import ticks_ms
-from helpers import detectar_saltos, iniciar_access_point
+from machine      import Pin
+from time         import ticks_ms
+from helpers      import detectar_saltos, iniciar_access_point
+import usocket
 
-# inicializando variables importantes ===
+
+# inicializando variables importantes
 
 infra_1 = Pin(25, Pin.IN)
 infra_2 = Pin(26, Pin.IN)
@@ -19,11 +21,25 @@ tiempo_espera = 5
 # iniciando access point para crear red wifi
 iniciar_access_point("LETRAMP-TIEMPO-VUELO-1", "123456789")
 
+sock_server = usocket.socket(usocket.AF_INET, usocket.SOCK_STREAM)
+sock_server.bind(('192.168.4.1', 8197))
+sock_server.listen(3)
+
 
 
 # estableciendo loop principal
 while True:
     try:
+        sc, addr = sock_server.accept()
+        print(f"direccion = {addr}")
+        while True:
+            mensaje = sc.recv(64)
+            print(mensaje)
+            if not mensaje:
+                break
+        sc.close()
+            
+        
         if ticks_ms() - tiempo_previo >= tiempo_espera:
             tiempo_previo = ticks_ms()
             
@@ -31,5 +47,6 @@ while True:
             estado_anterior_2, contador_2 = detectar_saltos(estado_anterior_2, infra_2, contador_2, "amarillo")
             
     except:
+        sock_server.close()
+        print("conexion finalizada ")
         break
-
